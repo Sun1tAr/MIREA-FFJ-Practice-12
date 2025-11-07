d6887d46697470b228cd0cf0b8cba26ce8a3e486
Sun1tAr <denchik2376@yandex.ru>
2025-11-07 18:38:25 +0300

    Initial commit

---
+++ src/main/java/my/learn/mireaffjpractice12/MireaFfjPractice12Application.java
@@ -0,0 +1,13 @@
+package my.learn.mireaffjpractice12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MireaFfjPractice12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MireaFfjPractice12Application.class, args);
+    }
+
+}
---
+++ src/test/java/my/learn/mireaffjpractice12/MireaFfjPractice12ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.learn.mireaffjpractice12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MireaFfjPractice12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
